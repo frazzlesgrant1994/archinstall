b6c5d62cbd33386398173f4620bc98a0e3ec268f
frazzlesgrant1994 <39445054+frazzlesgrant1994@users.noreply.github.com>
2020-12-03 01:01:58 +0000

    Update terminal.sh

--- terminal.sh
+++ terminal.sh
@@ -1,30 +1,25 @@
 #!/bin/bash		
 
-##Download from owncloud server			
-			 curl https://storage.smartchoiceitsolutions.co.uk:8443/owncloud/s/LfzPNLF1G0dnoNy/download --output terminal.tar.gz
+
         
             		
 					
 ## Make Temp folder to extrack backup		
 	        	
-			mkdir tmpter
-			
-## Extract Backip to temp folder		    
-			tar -xvf terminal.tar.gz  -C tmpter/
+
 			
 			
 ## Sync backup back to home dir 			
-			cd tmpter/input/
+			cd terminal/
 			
 			makepkg -si		
              
 ## Removes tar file			 
 			cd ..
-			cd ..
+			
 			
 ## Removes temp folder			
-		   	rm -rf tmpter/*
-		    	rm -rf tmpter
+		   
 			clear			
 		
 		
