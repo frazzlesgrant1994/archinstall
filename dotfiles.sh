58124c7ef55c82b561e8d3453be83a7133a404c6
frazzlesgrant1994 <39445054+frazzlesgrant1994@users.noreply.github.com>
2020-12-08 16:33:11 +0000

    Update dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -8,31 +8,21 @@ echo '	WARNING!! Running this will delete current config.'
     tar -zcvf ~/backup.tar.gz  ~/.[^.]*
  fi
 
-##Download from owncloud server			
+	
 			
-        
+## Removes current config        
             		rm -rf ~/.[^.]*
-			#sudo rm -rf /etc/skel/.[^.]*
-			
-## Make Temp folder to extrack backup		
-	        	
-			
-			
-## Extract Backip to temp folder		    
+
+					    
 			
 			
 			
 ## Sync backup back to home dir 			
 			rsync -avzP dotfiles/.[^.]* ~/
-			#sudo rsync -avzP temp/temp/.[^.]* /etc/skel/
+
 		
              
-## Removes tar file			 
-			
-			
-## Removes temp folder			
-		   	rm -rf temp/*
-		    	rm -rf temp
+
 			clear			
 		
 		
